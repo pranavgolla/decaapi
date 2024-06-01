693bb169b1b8c70a54e3288498a338b17286d2cd
pranavgolla <hlcyouth6@gmail.com>
2024-06-01 22:30:27 +0530

    decaverse

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
