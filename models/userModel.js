180439c26ca1b977213cd6b5e8aaa2a4e2f8c3a2
pranavgolla <142439361+pranavgolla@users.noreply.github.com>
2024-07-09 21:54:52 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    mobile:{type:String,required:true},
     password: { type: String },
     otp: { type: String },
     otpExpires: { type: Date }
